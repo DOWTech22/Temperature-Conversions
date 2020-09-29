b265da56cf3ee05e517e0511c8499e289cb0c7e2
Danilo O Williams <DWOshio@gmail.com>
2020-09-29 03:23:48 -0400

    Add project files.

---
+++ Temperature Conversions/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Temperature_Conversions
+{
+    class Program
+    {
+            // A short bit of code easy code.
+
+        static void Main(string[] args)
+        {
+            //using a double type because I am using a decimal(1.8) in my code.
+            double celsius, fahrenheit, kelvin;
+
+
+            Console.Write("Enter the amount of Celsius: ");
+            celsius = Convert.ToDouble(Console.ReadLine());
+
+            // formula
+            kelvin = celsius + 273;
+            fahrenheit = 1.8 * (celsius) + 32;
+
+            // Displays output.
+            Console.WriteLine("Kelvin = {0}", kelvin);
+            Console.WriteLine("Fahrenheit = {0}", fahrenheit);
+
+
+        }
+    }
+}
